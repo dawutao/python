0b054ebc76ed416a99ea25239dfc8c419f882256
呉涛 <wutao890214@icloud.com>
2022-11-11 17:14:55 +0900

    test

--- alien.py
+++ alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
 
         super().__init__()
+
         self.screen = ai_game.screen
 
         self.image = pygame.image.load('images/alien.bmp')
